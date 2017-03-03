255e475937224496bd49259c77a6449fc58bd3b8
井上正裕 <inouemasahiro@inouemasahiro-no-macbook-air.local>
2017-03-03 19:49:00 +0900

    first commit

--- carshareprojectfinal/ViewController.swift
+++ carshareprojectfinal/ViewController.swift
@@ -1,25 +1,21 @@
 //
 //  ViewController.swift
-//  carshareprojectfinal
+//  CustomStuff
 //
-//  Created by 井上正裕 on 2017/03/03.
-//  Copyright © 2017年 井上正裕. All rights reserved.
+//  Created by Evan Dekhayser on 7/9/14.
+//  Copyright (c) 2014 Evan Dekhayser. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func onBurger() {
+        (tabBarController as! TabBarController).sidebar.showInViewController(self, animated: true)
     }
-
-
 }
 
